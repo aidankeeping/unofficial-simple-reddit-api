347e0e58ef2c4e1b389800bf1f52be8eb0ff0e64
Aidan Madsen <amadsen185@gmail.com>
2019-08-27 19:51:01 -0700

    First Commit!

---
+++ app.js
@@ -0,0 +1,24 @@
+const rs = require('reddit-simple').RedditSimple;
+const app = require('express')();
+
+app.get('/api/:sub', (req, res) => {
+    rs.RandomPost(req.params.sub).then(x => {
+        let num = Math.floor(Math.random() * x.length);
+        res.send({title: x[num].data.title, body: x[num].data.selftext, author: x[num].data.author});
+    }).catch(err => {
+        return;
+    });
+});
+
+app.get('/:sub', (req, res) => {
+    rs.RandomPost(req.params.sub).then(x => {
+        let num = Math.floor(Math.random() * x.length);
+        res.send(`${x[num].data.title} ${x[num].data.selftext}`);
+    }).catch(err => {
+        return;
+    });
+});
+
+app.listen(80, '192.168.0.14', () => {
+    console.log("Web Server Has Started.");
+});
